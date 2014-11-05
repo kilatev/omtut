1d5653bcbe948103e3ac59ecf346ef52edccf453
Vitalijs Ozornins <vetalik@gmail.com>
2014-11-05 17:04:55 +0200

    gogogo

--- om-tut/src/om_tut/core.cljs
+++ om-tut/src/om_tut/core.cljs
@@ -2,7 +2,9 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]))
+            [cljs.core.async :refer [put! chan <!]]
+            [clojure.data :as data]
+            [clojure.string :as string]))
 
 (enable-console-print!)
 
@@ -20,22 +22,46 @@
   (reify
     om/IInitState
     (init-state [_]
-      {:delete (chan)})
+      {:delete (chan)
+       :text ""})
     om/IWillMount
     (will-mount [_]
       (let [delete (om/get-state owner :delete)]
         (go (loop []
-          (let [contact (<! delete)]
-            (om/transact! app :contacts
-              (fn [xs] (vec (remove #(= contact %) xs))))
-            (recur))))))
+              (let [contact (<! delete)]
+                (om/transact! app :contacts
+                  (fn [xs] (vec (remove #(= contact %) xs))))
+                (recur))))))
     om/IRenderState
-    (render-state [this {:keys [delete]}]
+    (render-state [this state]
       (dom/div nil
         (dom/h2 nil "Contact list")
         (apply dom/ul nil
           (om/build-all contact-view (:contacts app)
-            {:init-state {:delete delete}}))))))
+            {:init-state state}))
+        (dom/div nil
+          (dom/input
+            #js {:type "text" :ref "new-contact" :value (:text state)
+                 :onChange #(handle-change % owner state)})
+          (dom/button #js {:onClick #(add-contact app owner)} "Add contact"))))))
+(defn parse-contact [contact-str]
+  (let [[first middle last :as parts] (string/split contact-str #"\s+")
+        [first last middle] (if (nil? last) [first middle] [first last middle])
+        middle (when middle (string/replace middle "." ""))
+        c (if middle (count middle) 0)]
+    (when (>= (count parts) 2)
+      (cond-> {:first first :last last}
+        (== c 1) (assoc :middle-initial middle)
+        (>= c 2) (assoc :middle middle)))))
+
+(parse-contact "Gerald J. Sussman")
+
+(defn add-contact [app owner]
+  (let [new-contact (-> (om/get-node owner "new-contact")
+                        .-value
+                        parse-contact)]
+    (when new-contact
+      (om/transact! app :contacts #(conj % new-contact)))))
 
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
